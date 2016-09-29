4f040e2f1cfdc9725d5ca8d9110efec536d22302
maria <maria@eldis.ru>
2016-09-29 16:48:07 +0700

    async processes in redux-app

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 var path = require('path')
 var webpack = require('webpack')
+var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -15,7 +18,8 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new NpmInstallPlugin()
   ],
   module: {
     preLoaders: [
@@ -35,7 +39,14 @@ module.exports = {
         ],
         test: /\.js$/,
         plugins: ['transform-runtime'],
+      },
+      {
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
       }
-    ]
+    ],
+    postcss: function () {
+      return [autoprefixer, precss];
+    }
   }
 }
